c7fa1febd3405baf2fb6418c80dc20f63966b5b5
VivekReddyPalsani <138858031+VivekReddyPalsani@users.noreply.github.com>
2025-06-09 15:43:47 +0530

    .

---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+export default function Header({ name }) {
+  return (
+    <div className="App-Header-Row" style={{ backgroundColor: "orange" }}>
+      <div>
+        <h1>{name}</h1>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/"><button>Home</button></Link>
+          </li>
+          <li>
+            <Link to="/login"><button>Login</button></Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/Register.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+export default function Register() {
+  const [user, setUser] = useState({});
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(0);
+  const handleClick = () => {
+    alert("Hello World");
+  };
+  const updateCount = () => {
+    setCount(count + 1);
+  };
+  const addToValue = () => {
+    setValue(value + 1);
+  }
+  const subToValue = () => {
+    setValue(value - 1);
+  }
+  return (
+    <div>
+      <h2>Register</h2>
+      <p>
+        <input
+          type="text"
+          placeholder="Enter Name"
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
+      </p>
+      <p>
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Enter Email Address"
+        />
+      </p>
+      <p>
+        <input
+          type="password"
+          onChange={(e) => setUser({ ...user, pass: e.target.value })}
+          placeholder="New Password"
+        />
+      </p>
+      <p>
+        <button>Submit</button>
+      </p>
+      <hr />
+      <p>
+        <Link to="/login">Aready a member? Login Here...</Link>
+      </p>
+      <hr />
+      <button onClick={handleClick}>Click</button>
+      <hr />
+      <p>
+        {count}<br></br>
+        <button onClick={updateCount}>Update Count</button>
+      </p>
+      <div style={{display:'inline-flex', margin:20}}>
+        <button onClick={subToValue}> - </button>
+        <p>{value}</p>
+        <button onClick={addToValue}> + </button>
+      </div>
+    </div>
+  );
+}
